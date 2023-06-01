111881d36f15d95fd286e1610ed8ed2e0b7cb01e
Huynh Thanh Dat <huynhthanhdat1108@gmail.com>
2023-06-02 00:33:30 +0700

    update config

--- apis/configs/config.js
+++ apis/configs/config.js
@@ -1,14 +1,14 @@
 // Production
-// export const config = {
-//     SFU_ADDRESS:"wss://smartss.click/controller/ws",
-//     BASE_URL : "https://smartss.click",
-//     BASE_URL_PATH : "/api-gateway",
-// }
+export const config = {
+    SFU_ADDRESS:"wss://smartss.click/controller/ws",
+    BASE_URL : "https://smartss.click",
+    BASE_URL_PATH : "/api-gateway",
+}
 
 
 // Local
-export const config = {
-    SFU_ADDRESS: "ws://192.168.2.68:8080/ws",
-    BASE_URL: "http://192.168.2.68:3001",
-    BASE_URL_PATH: "",
-}
+// export const config = {
+//     SFU_ADDRESS: "ws://192.168.2.68:8080/ws",
+//     BASE_URL: "http://192.168.2.68:3001",
+//     BASE_URL_PATH: "",
+// }
